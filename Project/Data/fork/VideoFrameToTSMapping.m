c9e3a352fc40269d08806425f5a33a46d9fd0e2a
ashishsirohi <ashishsirohi@live.com>
2017-10-20 13:21:55 -0700

    All 4 eating classified data added

--- Project/Data/fork/VideoFrameToTSMapping.m
+++ Project/Data/fork/VideoFrameToTSMapping.m
@@ -1,9 +1,9 @@
-vidObj = VideoReader('1503609551913.mp4');
+vidObj = VideoReader('1503610324174.mp4');
 numFrames = ceil(vidObj.FrameRate*vidObj.Duration); %number of frames
 vidDuration = vidObj.Duration * 1000; %duration in millisecond
 NumMsPerFrame = floor(vidDuration/numFrames); 
-outFile = fopen('VideoFrameToTSMapping_EMG.txt','w'); % No of millisecodns per Frame
-lastFrameTS =  1503609785514; %TS of last video frame in IMU file
+outFile = fopen('VideoFrameToTSMapping_IMU.txt','w'); % No of millisecodns per Frame
+lastFrameTS =  1503610583338; %TS of last video frame in IMU file
 
 while numFrames > 0
     fprintf(outFile, "%d, %d", numFrames, lastFrameTS);
--- Project/Data/fork/classification_EMG.m
+++ Project/Data/fork/classification_EMG.m
@@ -1,7 +1,8 @@
-fid = fopen('1503609551913.txt');
+fid = fopen('1503610324174.txt');
 tline = fgetl(fid);
-outFile = fopen('EatingData_IMU.txt','w');
 %{
+outFile = fopen('EatingData_EMG.txt','w');
+{
 for c=1:4
     s = "EA";
    fprintf(outFile, "%s%d, %s\n", s, c, "OX");
@@ -15,57 +16,49 @@ for c=1:4
    fprintf(outFile, "%s%d, %s\n", s, c, "GY");
    fprintf(outFile, "%s%d, %s\n", s, c, "GZ");
  end
- %}
+ 
 fclose(outFile);
-EA1OX = ["EA1" "OX"];
-EA1OY = ["EA1" "OY"];
-EA1OZ = ["EA1" "OZ"];
-EA1OW = ["EA1" "OW"];
-EA1AX = ["EA1" "AX"];
-EA1AY = ["EA1" "AY"];
-EA1AZ = ["EA1" "AZ"];
-EA1GX = ["EA1" "GX"];
-EA1GY = ["EA1" "GY"];
-EA1GZ = ["EA1" "GZ"];
-EA2OX = ["EA2" "OX"];
-EA2OY = ["EA2" "OY"];
-EA2OZ = ["EA2" "OZ"];
-EA2OW = ["EA2" "OW"];
-EA2AX = ["EA2" "AX"];
-EA2AY = ["EA2" "AY"];
-EA2AZ = ["EA2" "AZ"];
-EA2GX = ["EA2" "GX"];
-EA2GY = ["EA2" "GY"];
-EA2GZ = ["EA2" "GZ"];
-EA3OX = ["EA3" "OX"];
-EA3OY = ["EA3" "OY"];
-EA3OZ = ["EA3" "OZ"];
-EA3OW = ["EA3" "OW"];
-EA3AX = ["EA3" "AX"];
-EA3AY = ["EA3" "AY"];
-EA3AZ = ["EA3" "AZ"];
-EA3GX = ["EA3" "GX"];
-EA3GY = ["EA3" "GY"];
-EA3GZ = ["EA3" "GZ"];
-EA4OX = ["EA4" "OX"];
-EA4OY = ["EA4" "OY"];
-EA4OZ = ["EA4" "OZ"];
-EA4OW = ["EA4" "OW"];
-EA4AX = ["EA4" "AX"];
-EA4AY = ["EA4" "AY"];
-EA4AZ = ["EA4" "AZ"];
-EA4GX = ["EA4" "GX"];
-EA4GY = ["EA4" "GY"];
-EA4GZ = ["EA4" "GZ"];
+%}
+EA1OX = ["EA1" "EMG1"];
+EA1OY = ["EA1" "EMG2"];
+EA1OZ = ["EA1" "EMG3"];
+EA1OW = ["EA1" "EMG4"];
+EA1AX = ["EA1" "EMG5"];
+EA1AY = ["EA1" "EMG6"];
+EA1AZ = ["EA1" "EMG7"];
+EA1GX = ["EA1" "EMG8"];
+EA2OX = ["EA2" "EMG1"];
+EA2OY = ["EA2" "EMG2"];
+EA2OZ = ["EA2" "EMG3"];
+EA2OW = ["EA2" "EMG4"];
+EA2AX = ["EA2" "EMG5"];
+EA2AY = ["EA2" "EMG6"];
+EA2AZ = ["EA2" "EMG7"];
+EA2GX = ["EA2" "EMG8"];
+EA3OX = ["EA3" "EMG1"];
+EA3OY = ["EA3" "EMG2"];
+EA3OZ = ["EA3" "EMG3"];
+EA3OW = ["EA3" "EMG4"];
+EA3AX = ["EA3" "EMG5"];
+EA3AY = ["EA3" "EMG6"];
+EA3AZ = ["EA3" "EMG7"];
+EA3GX = ["EA3" "EMG8"];
+EA4OX = ["EA4" "EMG1"];
+EA4OY = ["EA4" "EMG2"];
+EA4OZ = ["EA4" "EMG3"];
+EA4OW = ["EA4" "EMG4"];
+EA4AX = ["EA4" "EMG5"];
+EA4AY = ["EA4" "EMG6"];
+EA4AZ = ["EA4" "EMG7"];
+EA4GX = ["EA4" "EMG8"];
 while ischar(tline)
     %disp(tline);
     data = allwords(tline);
     disp(data(1,1))
     disp(data(1,2))
     disp(data(1,3))
-    ea = strcat('EA', data(1,3));
     tline = fgetl(fid);
-    ts = fopen('VideoFrameToTSMapping_IMU.txt');
+    ts = fopen('VideoFrameToTSMapping_EMG.txt');
     tsline = fgetl(ts);
     while ischar(tsline)
         tsline = fgetl(ts);
@@ -79,7 +72,7 @@ while ischar(tline)
     fclose(ts);
     disp(ts1);
     disp(ts2);
-    imu = fopen('1503609551913_IMU.txt');
+    imu = fopen('1503610324174_EMG.txt');
     imuline = fgetl(imu);
     while ischar(imuline)
         imulinesplit = allwords(imuline);
@@ -97,8 +90,6 @@ while ischar(tline)
                 EA1AY = [EA1AY imusplit(1,7)];
                 EA1AZ = [EA1AZ imusplit(1,8)];
                 EA1GX = [EA1GX imusplit(1,9)];
-                EA1GY = [EA1GY imusplit(1,10)];
-                EA1GZ = [EA1GZ imusplit(1,11)];
             elseif data(1,3) == "2"
                 EA2OX = [EA2OX imusplit(1,2)];
                 EA2OY = [EA2OY imusplit(1,3)];
@@ -108,8 +99,6 @@ while ischar(tline)
                 EA2AY = [EA2AY imusplit(1,7)];
                 EA2AZ = [EA2AZ imusplit(1,8)];
                 EA2GX = [EA2GX imusplit(1,9)];
-                EA2GY = [EA2GY imusplit(1,10)];
-                EA2GZ = [EA2GZ imusplit(1,11)];
             elseif data(1,3) == "3"
                 EA3OX = [EA3OX imusplit(1,2)];
                 EA3OY = [EA3OY imusplit(1,3)];
@@ -119,8 +108,6 @@ while ischar(tline)
                 EA3AY = [EA3AY imusplit(1,7)];
                 EA3AZ = [EA3AZ imusplit(1,8)];
                 EA3GX = [EA3GX imusplit(1,9)];
-                EA3GY = [EA3GY imusplit(1,10)];
-                EA3GZ = [EA3GZ imusplit(1,11)];
             elseif data(1,3) == "4"
                 EA4OX = [EA4OX imusplit(1,2)];
                 EA4OY = [EA4OY imusplit(1,3)];
@@ -130,8 +117,6 @@ while ischar(tline)
                 EA4AY = [EA4AY imusplit(1,7)];
                 EA4AZ = [EA4AZ imusplit(1,8)];
                 EA4GX = [EA4GX imusplit(1,9)];
-                EA4GY = [EA4GY imusplit(1,10)];
-                EA4GZ = [EA4GZ imusplit(1,11)];
             end
             %ea1 = [ea1 imusplit(1,2)];
             
@@ -141,7 +126,7 @@ while ischar(tline)
     fclose(imu);
     %break;
 end
-res = fopen("EatingData_IMU.txt", 'w');
+res = fopen("EatingData_EMG.txt", 'w');
 fprintf(res, "%s, ", EA1OX);
 fprintf(res, "\n");
 fprintf(res, "%s, ", EA1OY);
@@ -157,10 +142,6 @@ fprintf(res, "\n");
 fprintf(res, "%s, ", EA1AZ);
 fprintf(res, "\n");
 fprintf(res, "%s, ", EA1GX);
-fprintf(res, "\n");
-fprintf(res, "%s, ", EA1GY);
-fprintf(res, "\n");
-fprintf(res, "%s, ", EA1GZ);
 fprintf(res, "\n");
 
 fprintf(res, "%s, ", EA2OX);
@@ -179,10 +160,6 @@ fprintf(res, "%s, ", EA2AZ);
 fprintf(res, "\n");
 fprintf(res, "%s, ", EA2GX);
 fprintf(res, "\n");
-fprintf(res, "%s, ", EA2GY);
-fprintf(res, "\n");
-fprintf(res, "%s, ", EA2GZ);
-fprintf(res, "\n");
 
 fprintf(res, "%s, ", EA3OX);
 fprintf(res, "\n");
@@ -200,10 +177,6 @@ fprintf(res, "%s, ", EA3AZ);
 fprintf(res, "\n");
 fprintf(res, "%s, ", EA3GX);
 fprintf(res, "\n");
-fprintf(res, "%s, ", EA3GY);
-fprintf(res, "\n");
-fprintf(res, "%s, ", EA3GZ);
-fprintf(res, "\n");
 
 fprintf(res, "%s, ", EA4OX);
 fprintf(res, "\n");
@@ -220,10 +193,6 @@ fprintf(res, "\n");
 fprintf(res, "%s, ", EA4AZ);
 fprintf(res, "\n");
 fprintf(res, "%s, ", EA4GX);
-fprintf(res, "\n");
-fprintf(res, "%s, ", EA4GY);
-fprintf(res, "\n");
-fprintf(res, "%s, ", EA4GZ);
 fprintf(res, "\n");
 %{
 for k=1:4
